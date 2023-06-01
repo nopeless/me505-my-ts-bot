142919c3c584e5b1b5f0f1a15a169810a2f98015
me505 <Jainarjav886@gmail.com>
2023-06-01 23:49:11 +0530

    made a test case finally

---
+++ tests/commands.test.ts
@@ -0,0 +1,21 @@
+const { Client, Message } = require('discord.js');
+const ping = require('./../src/commands/ping.ts')
+describe('ping command', () => {
+  let message: any;
+
+  beforeEach(() => {
+    message = {
+      content: '/ping',
+      reply: jest.fn(),
+    };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should reply with pong!', () => {
+    ping.execute(message)
+    expect(message.reply).toHaveBeenCalledWith('pong!');
+  });
+});
