51bf358035de53a46e5fde6e531af82a51662429
nopeless <38830903+nopeless@users.noreply.github.com>
2023-06-03 00:35:53 +0900

    refactor: better assertions

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,10 @@
 import { Events } from "discord.js"
+import { EventModule } from "../types.js"
+
 export default {
   name: Events.ClientReady,
   once: true,
   async execute() {
     console.log("The bot is ready")
   }
-}
+} satisfies EventModule
