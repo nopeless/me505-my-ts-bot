ba83185cc7b4e89e2cdf2c58ddd0e0bcafd3f413
nopeless <38830903+nopeless@users.noreply.github.com>
2023-06-02 23:30:48 +0900

    chore: improve gitignore

--- dist/commands/ping.js
+++
@@ -1,7 +0,0 @@
-const { Message } = require("discord.js");
-export default {
-    name: "ping",
-    async execute(msg) {
-        msg.reply("pong!");
-    }
-};
--- dist/events/ready.js
+++
@@ -1,8 +0,0 @@
-import { Events } from "discord.js";
-export default {
-    name: Events.ClientReady,
-    once: true,
-    async execute() {
-        console.log("The bot is ready");
-    }
-};
