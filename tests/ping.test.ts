0c7af7a79ff23af61d564eb8c82b5f04750a8baa
nopeless <38830903+nopeless@users.noreply.github.com>
2023-06-02 23:41:29 +0900

    test: configure test directory

--- tests/ping.test.ts
+++ tests/ping.test.ts
@@ -1,25 +1,6 @@
-// import { Message } from 'discord.js';
-import { Default } from '../src/commands/ping.ts'
-console.log(Default)
-describe('ping command', () => {
-  let message: any;
 
-  beforeEach(() => {
-    message = {
-      content: '/ping',
-      reply: jest.fn(),
-    };
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it('should reply with pong!', () => {
-    Default["execute"](message)
-    console.log(message)
-    expect(message.reply).toHaveBeenCalledWith('pong!');
-  });
-});
-let Default = { func: describe }
-export { Default }
+describe("main", () => {
+  test("some functionality", () => {
+    expect(true).to.be.true;
+  })
+})
